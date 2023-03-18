92d9ec4eab0d9bb3300f48561e71b846392c7894
AbdurNashith <www.nashith90@gmail.com>
2023-03-18 16:55:56 +0530

    initial commit

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -3,37 +3,47 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred,{
-    #'databaseURL':"Enter RealTime URL here"
+firebase_admin.initialize_app(cred, {
+	'databaseURL': ""
 })
-
 
 ref = db.reference('Person')
 
 data = {
-    "111111":
-        {
-            "name": "Elon Musk",
-            "major": "Aerospace",
-            "starting_year": 2017,
-            "total_attendance": 5,
-            "standing": "S",
-            "year": 4,
-            "last_attendance_time": "2023-03-12 02:03:22"
+	"111111":
+		{
+			"name": "Elon Musk",
+			"major": "CSE",
+			"starting_year": 2017,
+			"total_attendance": 5,
+			"standing": "S",
+			"year": 4,
+			"last_attendance_time": "2023-03-12 02:03:22"
 
-        },
-    "333333":
-        {
-            "name": "Abdur Rahman Nashith C",
-            "major": "IT",
-            "starting_year": 2020,
-            "total_attendance": 5,
-            "standing": "S",
-            "year": 4,
-            "last_attendance_time": "2023-03-12 02:03:22"
+		},
+	"333333":
+		{
+			"name": "Nashith",
+			"major": "IT",
+			"starting_year": 2020,
+			"total_attendance": 5,
+			"standing": "S",
+			"year": 4,
+			"last_attendance_time": "2023-03-12 02:03:22"
 
-        }
+		},
+	"222222":
+		{
+			"name": "Laura Morano",
+			"major": "Tailoring",
+			"starting_year": 2002,
+			"total_attendance": 7,
+			"standing": "A",
+			"year": 4,
+			"last_attendance_time": "2023-03-12 02:03:22"
+
+		}
 }
 
-for key,value in data.items():
-    ref.child(key).set(value)
+for key, value in data.items():
+	ref.child(key).set(value)
